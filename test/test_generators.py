fc6f781737bf3250fe2fa589ff8d6f57c914447f
lobanov <lav1986@inbox.ru>
2025-03-02 03:16:57 +0300

    Доработка тестов модуля generators

--- test/test_generators.py
+++ test/test_generators.py
@@ -10,45 +10,60 @@ def test_filter_by_currency_param_executed() -> None:
     usd_transactions = filter_by_currency(fix_generators_data_test, "USD")
     rub_transactions = filter_by_currency(fix_generators_data_test, "RUB")
 
-    assert next(usd_transactions) == fix_generators_data_usd_test[0]
+    assert next(usd_transactions, "") == fix_generators_data_usd_test[0]
 
-    assert next(usd_transactions) == fix_generators_data_usd_test[1]
+    assert next(usd_transactions, "") == fix_generators_data_usd_test[1]
 
-    assert next(usd_transactions) == fix_generators_data_usd_test[2]
+    assert next(usd_transactions, "") == fix_generators_data_usd_test[2]
 
-    assert next(rub_transactions) == fix_generators_data_rub_test[0]
+    assert next(rub_transactions, "") == fix_generators_data_rub_test[0]
 
-    assert next(rub_transactions) == fix_generators_data_rub_test[1]
+    assert next(rub_transactions, "") == fix_generators_data_rub_test[1]
+
+def test_filter_by_currency_param_no_value() -> None:
+
+    it_transactions = filter_by_currency(fix_generators_data_test, "UK")
+
+    assert next(it_transactions,"") == ""
+
+    it_transactions = filter_by_currency([], "UK")
+    assert next(it_transactions, "") == ""
 
 def test_transaction_descriptions() -> None:
     it_transactions_descriptions = transaction_descriptions(fix_generators_data_test)
 
-    assert next(it_transactions_descriptions) == "Перевод организации"
+    assert next(it_transactions_descriptions, "") == "Перевод организации"
 
-    assert next(it_transactions_descriptions) == "Перевод со счета на счет"
+    assert next(it_transactions_descriptions, "") == "Перевод со счета на счет"
 
-    assert next(it_transactions_descriptions) == "Перевод со счета на счет"
+    assert next(it_transactions_descriptions, "") == "Перевод со счета на счет"
 
-    assert next(it_transactions_descriptions) == "Перевод с карты на карту"
+    assert next(it_transactions_descriptions, "") == "Перевод с карты на карту"
 
-    assert next(it_transactions_descriptions) == "Перевод организации"
+    assert next(it_transactions_descriptions, "") == "Перевод организации"
+
+def test_transaction_descriptions_empy() -> None:
+    it_transactions_descriptions = transaction_descriptions([])
+
+    assert next(it_transactions_descriptions, "") == ""
+
 
 def test_def_card_number_generator() -> None:
     it_card_number_generator = card_number_generator(1, 5)
 
-    assert next(it_card_number_generator) == "0000 0000 0000 0001"
-    assert next(it_card_number_generator) == "0000 0000 0000 0002"
-    assert next(it_card_number_generator) == "0000 0000 0000 0003"
-    assert next(it_card_number_generator) == "0000 0000 0000 0004"
-    assert next(it_card_number_generator) == "0000 0000 0000 0005"
+    assert next(it_card_number_generator, "") == "0000 0000 0000 0001"
+    assert next(it_card_number_generator, "") == "0000 0000 0000 0002"
+    assert next(it_card_number_generator, "") == "0000 0000 0000 0003"
+    assert next(it_card_number_generator, "") == "0000 0000 0000 0004"
+    assert next(it_card_number_generator, "") == "0000 0000 0000 0005"
 
     it_card_number_generator = card_number_generator(1000000000001, 1000000000005)
 
-    assert next(it_card_number_generator) == "0001 0000 0000 0001"
-    assert next(it_card_number_generator) == "0001 0000 0000 0002"
-    assert next(it_card_number_generator) == "0001 0000 0000 0003"
-    assert next(it_card_number_generator) == "0001 0000 0000 0004"
-    assert next(it_card_number_generator) == "0001 0000 0000 0005"
+    assert next(it_card_number_generator, "") == "0001 0000 0000 0001"
+    assert next(it_card_number_generator, "") == "0001 0000 0000 0002"
+    assert next(it_card_number_generator, "") == "0001 0000 0000 0003"
+    assert next(it_card_number_generator, "") == "0001 0000 0000 0004"
+    assert next(it_card_number_generator, "") == "0001 0000 0000 0005"
 
 def test_def_card_number_generator_wrong_arguments() -> None:
     """Проверка на ошибку при вызове с пустым аргументом"""
