565a9cbda0cf211878df26009df83a0fe7af76da
lobanov <lav1986@inbox.ru>
2025-03-08 22:36:28 +0300

    Реализация модуля external_api

---
+++ src/external_api.py
@@ -0,0 +1,71 @@
+import os
+import json
+from dotenv import load_dotenv
+import requests
+
+load_dotenv('.env')
+
+def get_exchangerates_data(amount: float, amount_from: str, amount_to: str) -> float:
+    """
+    Функция проводит конвертацию двух валют. С использованием API
+    :param amount:          - конвертируемая сумма.
+    :param amount_from:     - трехбуквенный код валюты, из которой происходит конвертация.
+    :param amount_to:       - трехбуквенный код валюты, в которую происходит конвертация.
+    :return:
+    """
+    headers = {'api-key': os.getenv('API_KEY')}
+    payload = {}
+    params = {'amount': amount, 'from': amount_from, 'to': amount_to}
+
+    try:
+        response = requests.request("GET", os.getenv('BASE_URL'), headers=headers, data=payload)
+        response.raise_for_status()
+    # запрос не может быть выполнен из-за проблем с сетью
+    except requests.exceptions.ConnectionError:
+        raise Exception("Connection Error. Please check your network connection.")
+    # запрос не получил ответа в течение заданного времени.
+    except requests.exceptions.Timeout:
+        raise Exception("Request timed out. Please check your internet connection.")
+    # количество перенаправлений запроса превышает максимально допустимое значение
+    except requests.exceptions.TooManyRedirects:
+        raise Exception("Too many redirects. Please check the URL.")
+    # полученный ответ от сервера не является корректным HTTP-ответом
+    except requests.exceptions.HTTPError:
+        raise Exception("HTTP Error. Please check the URL.")
+
+    data = json.loads(response.text)
+    # Если статус 200 -запрос прошел успешно
+    if response.status_code == 200:
+        return float(data["result"])
+    elif response.status_code == 400:
+        raise Exception("Bad Request. The request was unacceptable, often due to missing a required parameter.")
+    elif response.status_code == 401:
+        raise Exception("Unauthorized. Please check your API key.")
+    elif response.status_code == 403:
+        raise Exception("Forbidden. Please check your API key.")
+    elif response.status_code == 404:
+        raise Exception("Not Found. The requested resource doesn't exist.")
+    elif response.status_code == 429:
+        raise Exception("Too many requests. Please check the rate limit.")
+    elif response.status_code >= 500:
+        raise Exception("Server Error. We have failed to process your request. (You can contact us anytime)")
+
+
+def get_transaction_amount(transaction_data: dict) -> float:
+    """
+    Функция, принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float.
+    Если транзакция была в USD или EUR, происходит обращение к внешнему API для получения текущего курса валют
+    и конвертации суммы операции в рубли. Для конвертации валюты используется Exchange Rates Data API:
+    https://apilayer.com/exchangerates_data-api.
+    :param transaction_data:
+    :return:
+    """
+    # Если транзакция в рублях сразу возвращаю сумму транзакции в рублях
+    if transaction_data["operationAmount"]["currency"]["code"] == "RUB":
+        return float(transaction_data["operationAmount"]["amount"])
+    else:
+        # если транзакция не в рублях, то выполняю конвертацию в рубли и возвращаю значение
+        result = get_exchangerates_data(amount = float(transaction_data["operationAmount"]["amount"]),
+                               amount_from=transaction_data["operationAmount"]["currency"]["code"],
+                               amount_to="RUB")
+        return result
