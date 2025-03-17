d361a4f1a9983aada92f8b6d294cf604163a5e0a
lobanov <lav1986@inbox.ru>
2025-03-18 01:00:45 +0300

    Проверка логирования

--- src/main.py
+++ src/main.py
@@ -2,3 +2,10 @@
 # from src.widget import get_date
 # from src import generators
 # from src.generators import card_number_generator, filter_by_currency  # filter_by_currency, transaction_descriptions
+from src.utils import get_transaction_data
+from src.masks import get_mask_card_number, get_mask_account
+
+
+get_mask_card_number("0123456789012345")
+get_mask_account("0123456789012345")
+get_transaction_data("")
